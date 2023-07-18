c4bfe814e5eba6387dc427d53b9744aeca761bdf
Neoly Moreno <neolyamr@gmail.com>
2023-07-17 19:04:27 -0500

    Delete unnecessary lines in Execute class and Add descriptions to README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Yeralitas_Program
 Desktop program developed with JAVA with a MVC design pattern and CRUD to manage a Database in SQL SERVER
+
+Librerías:
+- AbsoluteLayout
+- Jcalendar
+- Javax.mail
+- Mssql 12.2.0
+
--- src/recursos/Execute.java
+++ src/recursos/Execute.java
@@ -4,13 +4,8 @@ import vistas.*;
 
 public class Execute {
 
-    public static void main(String[] args) {
-        Inicio_JFrame inicio = new Inicio_JFrame();
-        inicio.setVisible(true);
-
-        Calculadora_JFrame calculadora= Calculadora_JFrame.getCalculadora();
-        calculadora.setVisible(true);
-
-    }
-
+  public static void main(String[] args) {
+    Inicio_JFrame inicio = new Inicio_JFrame();
+    inicio.setVisible(true);
+  }
 }
